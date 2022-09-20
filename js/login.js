244b14be585bfa29c9246dcf4b4b4e0aef50d505
faguzzi <aurelio_guzzi@yahoo.com.br>
2022-09-19 22:48:44 -0300

    19_09_22

--- js/login.js
+++ js/login.js
@@ -1,19 +1,8 @@
-//quando a página estiver carregada por completo
 $(document).ready(function(){
-    // $("#email").val("Eduardo Costa")
-
-    // $("button").click(function(){
-    //     alert('Olá, botão clicado');        
-    // })
-
-
+    
     $("#signin").click( function(){
-        // let email = document.getElementById("email").value;
-        // let senha = document.getElementById("senha").value;
         let email = $("#email").val();
         let senha = $("#senha").val();
-
-        console.log( podeCadastrar(email, senha) );
 
         if( validarLogin(email, senha) ) {
             alert("Preenchido corretamente")
@@ -23,17 +12,12 @@ $(document).ready(function(){
 
     } )
 
-
-
-    function podeCadastrar(e, s) {
-        if( e == "" || s == "" ) {//sem preencher
-            return false;//avisa que não está tudo preenchido
+    function validarLogin(e, s) {
+        if( e == "" || s == "" ) {
+            return false;
         } else {
-            return true;//está tudo preenchido
+            return true;
         }
     }
-});// fim do jQuery Ready
-
-
-
+});
 
